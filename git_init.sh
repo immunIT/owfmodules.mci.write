0a09d6c1f69ed979485ea4ce28022912194c4006
ghecko <ghecko78@gmail.com>
2021-01-07 14:58:27 +0100

    Update git_init.sh

--- git_init.sh
+++ git_init.sh
@@ -1,3 +1,3 @@
-git remote set-url origin --push --add git@github.com:immunIT/<module_name>.git
-git remote set-url origin --push --add git@bitbucket.org:octowire/<module_name>.git
+git remote set-url origin --push --add git@github.com:immunIT/owfmodules.mci.write.git
+git remote set-url origin --push --add git@bitbucket.org:octowire/owfmodules.mci.write.git
 git remote -v
